4d32e06c1cbd310696a326eda4e72606bb2abb2b
Prof.Evaldo <evaldo.junior@etec.sp.gov.br>
2023-03-02 22:55:59 -0300

    NubankClone AppBar completa.

---
+++ nubankclone/lib/home_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          elevation: 0,
+          title: Container(
+              padding: EdgeInsets.all(6.0),
+              decoration:
+                  BoxDecoration(color: Colors.white24, shape: BoxShape.circle),
+              child: Icon(Icons.person_outline)),
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.visibility_outlined)),
+            IconButton(
+                onPressed: () {}, icon: Icon(Icons.help_outline_outlined))
+          ],
+          bottom: PreferredSize(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 18.0, bottom: 18),
+                    child: Text(
+                      "Prof. Evaldo",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+              preferredSize: Size.fromHeight(40))),
+      body: const Center(
+        child: Text('Hello World'),
+      ),
+    );
+  }
+}
