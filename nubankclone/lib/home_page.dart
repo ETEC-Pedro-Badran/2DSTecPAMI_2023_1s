45d8c31903ace8f2719bc8742abc35639c74aaba
Prof.Evaldo <evaldo.junior@etec.sp.gov.br>
2023-03-16 22:07:33 -0300

    Primeira página completa.

--- nubankclone/lib/home_page.dart
+++ nubankclone/lib/home_page.dart
@@ -1,7 +1,5 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:nubankclone/opcoes_widget.dart';
 
 class HomePage extends StatelessWidget {
@@ -37,43 +35,138 @@ class HomePage extends StatelessWidget {
                     ),
                   ],
                 ))),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 12),
+        body: SingleChildScrollView(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  Text(
-                    "Conta PJ",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                  ),
-                  Icon(
-                    Icons.keyboard_arrow_right_rounded,
-                    size: 20,
-                    color: Colors.black54,
-                  ),
-                ],
+              Container(
+                color: Colors.white,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 18.0, vertical: 12),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text(
+                          "Conta PJ",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 18),
+                        ),
+                        Icon(
+                          Icons.keyboard_arrow_right_rounded,
+                          size: 20,
+                          color: Colors.black54,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 18,
+                    ),
+                    const Text(
+                      r'R$ 2.500,52',
+                      style:
+                          TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      height: 25,
+                    ),
+                    SizedBox(
+                      height: 100,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: const [
+                          OpcaoWidget(icon: Icons.pix, rotulo: "Pix"),
+                          OpcaoWidget(
+                              icon: Icons.local_atm_rounded, rotulo: "Cobrar"),
+                          OpcaoWidget(
+                              icon: FontAwesomeIcons.barcode, rotulo: "Pagar"),
+                          OpcaoWidget(
+                              icon: Icons.price_change_outlined,
+                              rotulo: "Transferir"),
+                          OpcaoWidget(
+                              icon: Icons.add_card_outlined,
+                              rotulo: "Depositar"),
+                        ],
+                      ),
+                    ),
+                    Container(
+                        height: 70,
+                        decoration: BoxDecoration(
+                            color: Colors.grey[200],
+                            borderRadius: BorderRadius.circular(8)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: [
+                              Icon(Icons.credit_card_outlined),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Text("Meus cartões")
+                            ],
+                          ),
+                        ))
+                  ],
+                ),
               ),
-              const SizedBox(
-                height: 18,
+              Container(
+                color: Colors.grey[100],
+                height: 8,
               ),
-              const Text(
-                r'R$ 2.500,52',
-                style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(
-                height: 25,
-              ),
-              Row(
-                children: const [
-                  OpcaoWidget(icon: Icons.pix, rotulo: "Pix"),
-                  OpcaoWidget(icon: Icons.pix, rotulo: "Pix"),
-                  OpcaoWidget(icon: Icons.pix, rotulo: "Pix"),
-                  OpcaoWidget(icon: Icons.pix, rotulo: "Pix"),
-                  OpcaoWidget(icon: Icons.pix, rotulo: "Pix"),
-                ],
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 18.0, vertical: 12),
+                color: Colors.white,
+                child: Row(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: 10),
+                        Text(
+                          "Descubra Mais",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.w900),
+                        ),
+                        SizedBox(height: 20),
+                        Container(
+                          height: 200,
+                          width: 200,
+                          decoration: BoxDecoration(
+                              color: Colors.grey[200],
+                              borderRadius: BorderRadius.circular(8)),
+                          child: Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Chaves Pix",
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                Text(
+                                  "Simplifique a maneira de receber transferencias na sua conta PJ.",
+                                  style: TextStyle(fontSize: 12),
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                ElevatedButton(
+                                  onPressed: () {},
+                                  child: Text("Conhecer"),
+                                )
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
               )
             ],
           ),
--- nubankclone/lib/opcoes_widget.dart
+++ nubankclone/lib/opcoes_widget.dart
@@ -8,24 +8,29 @@ class OpcaoWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(right: 10.0),
-      child: Column(
-        children: [
-          Container(
-              padding: EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                  color: Colors.grey[300], shape: BoxShape.circle),
-              child: Icon(icon)),
-          const SizedBox(
-            height: 10,
+    return Column(
+      children: [
+        Container(
+          color: Colors.white,
+          padding: const EdgeInsets.only(right: 20.0),
+          child: Column(
+            children: [
+              Container(
+                  padding: EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                      color: Colors.grey[300], shape: BoxShape.circle),
+                  child: Icon(icon)),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                rotulo,
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              )
+            ],
           ),
-          Text(
-            rotulo,
-            style: const TextStyle(fontWeight: FontWeight.bold),
-          )
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
