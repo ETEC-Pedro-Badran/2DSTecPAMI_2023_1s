ad9a78322e614ec449164269231c02bc126c01a7
Evaldo <evaldo.junior@etec.sp.gov.br>
2023-04-18 20:24:42 -0300

    GridView de Produtos na HomePage

---
+++ loja/lib/product_model.dart
@@ -0,0 +1,86 @@
+import 'product_color_model.dart';
+
+class ProductList {
+  List<Product> products;
+
+  ProductList({required this.products});
+
+  factory ProductList.fromJson(List<dynamic> json) {
+    List<Product> products = json.map((i) => Product.fromJson(i)).toList();
+    return ProductList(products: products);
+  }
+}
+
+class Product {
+  int id;
+  String brand;
+  String name;
+  double price;
+  String priceSign;
+  String currency;
+  String imageLink;
+  String productLink;
+  String websiteLink;
+  String description;
+  double? rating;
+  String category;
+  String productType;
+  List<String> tagList;
+  DateTime createdAt;
+  DateTime updatedAt;
+  String productApiUrl;
+  String apiFeaturedImage;
+  List<ProductColor> productColors;
+
+  Product({
+    required this.id,
+    required this.brand,
+    required this.name,
+    required this.price,
+    required this.priceSign,
+    required this.currency,
+    required this.imageLink,
+    required this.productLink,
+    required this.websiteLink,
+    required this.description,
+    this.rating,
+    required this.category,
+    required this.productType,
+    required this.tagList,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.productApiUrl,
+    required this.apiFeaturedImage,
+    required this.productColors,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    List<ProductColor> productColors = [];
+    if (json['product_colors'] != null) {
+      productColors = (json['product_colors'] as List)
+          .map((i) => ProductColor.fromJson(i))
+          .toList();
+    }
+    return Product(
+      id: json['id'],
+      brand: json['brand'],
+      name: json['name'],
+      price: double.parse(json['price']),
+      priceSign: json['price_sign'],
+      currency: json['currency'],
+      imageLink: json['image_link'],
+      productLink: json['product_link'],
+      websiteLink: json['website_link'],
+      description: json['description'],
+      rating: json['rating'] != null ? double.parse(json['rating']) : null,
+      category: json['category'],
+      productType: json['product_type'],
+      tagList: List<String>.from(json['tag_list']),
+      createdAt: DateTime.parse(json['created_at']),
+      updatedAt: DateTime.parse(json['updated_at']),
+      productApiUrl: json['product_api_url'],
+      apiFeaturedImage: json['api_featured_image'],
+      productColors: productColors,
+    );
+  }
+}
---
+++ loja/lib/produto_tile.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class ProdutoTile extends StatelessWidget {
+  const ProdutoTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 2,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Image.network(
+              "https://a-static.mlcdn.com.br/618x463/smartphone-samsung-galaxy-a23-128gb-branco-4g-octa-core-4gb-ram-66-cam-quadrupla-selfie-8mp/magazineluiza/234749900/ca9fa16bf1cfd38bb5923eec9e2cfd53.jpg",
+              fit: BoxFit.contain,
+            ),
+            Text(
+              "Smartphone Samsung Galaxy A23 128GB Branco 4G - Octa-Core 4GB RAM 6,6” Câm Quádrupla + Selfie 8MP",
+              maxLines: 4,
+              overflow: TextOverflow.ellipsis,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Column(
+                  children: [
+                    Text("Por"),
+                    Text(
+                      r"R$",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.blue[900]),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Text(
+                    "1.199",
+                    style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.blue[900]),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(onPressed: () {}, child: Text("Comprar"))
+          ],
+        ),
+      ),
+    );
+  }
+}
