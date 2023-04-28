9a097ecee31bd2bb1e7d60fba4c797429e5fbeef
Evaldo <evaldo.junior@etec.sp.gov.br>
2023-04-27 22:38:23 -0300

    Teste unitario

--- loja/lib/product_rest.dart
+++ loja/lib/product_rest.dart
@@ -1,11 +1,19 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:loja/product_model.dart';
 
 class ProductRest {
   obterProdutos(http.Client client) {
     return client
         .get(Uri.parse("http://makeup-api.herokuapp.com/api/v1/products.json"))
         .then((value) {
-      print(value);
+      if (value.statusCode != 200) {
+        throw "Recurso não encontrado.";
+      } else {
+        List<dynamic> dados = json.decode(value.body);
+        return dados.map((e) => Product.fromJson(e)).toList();
+      }
     });
   }
 }
